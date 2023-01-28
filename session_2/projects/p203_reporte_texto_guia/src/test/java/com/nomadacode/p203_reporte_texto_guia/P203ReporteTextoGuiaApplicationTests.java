06c4c1b0c894ca6f9c2cf986d009c429603cc5f9
Alan Badillo Salas <dragonnomada123@gmail.com>
2023-01-28 11:43:25 -0600

    Session 2

---
+++ session_2/projects/p203_reporte_texto_guia/src/test/java/com/nomadacode/p203_reporte_texto_guia/P203ReporteTextoGuiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomadacode.p203_reporte_texto_guia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P203ReporteTextoGuiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
